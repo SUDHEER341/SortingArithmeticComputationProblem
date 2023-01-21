ca5b3e658410ca669552ea1e902901b063a0defd
SUDHEER341 <sudheer601220@gmail.com>
2023-01-21 12:30:32 +0530

    Add: Calculation of num1 * num2 + num3

--- sorting.sh
+++ sorting.sh
@@ -3,3 +3,4 @@ read -p "Enter second number: " num2
 read -p "Enter third number: " num3
 echo "The given three numbers are: $num1 $num2 $num3"
 echo "$num1 + $num2 * $num3 = $(( num1 + num2 * num3 ))"
+echo "$num1 + $num2 * $num3 = $(( num1 * num2 + num3 ))"
