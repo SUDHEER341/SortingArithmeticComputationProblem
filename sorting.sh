2d694ecd25cb1df47d8396185e97fdc77ef2b0ef
SUDHEER341 <sudheer601220@gmail.com>
2023-01-21 12:40:02 +0530

    Add: Calculation of num1 + num2 / num3

--- sorting.sh
+++ sorting.sh
@@ -3,4 +3,5 @@ read -p "Enter second number: " num2
 read -p "Enter third number: " num3
 echo "The given three numbers are: $num1 $num2 $num3"
 echo "$num1 + $num2 * $num3 = $(( num1 + num2 * num3 ))"
-echo "$num1 + $num2 * $num3 = $(( num1 * num2 + num3 ))"
+echo "$num1 * $num2 + $num3 = $(( num1 * num2 + num3 ))"
+echo "$num1 + $num2 / $num3 = $(( num1 + num2 / num3 ))"
